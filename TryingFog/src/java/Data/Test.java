2edab0fdccad1ddc295a63f7fc41b5313a86d031
Petru <catanapetru@gmail.com>
2017-04-18 10:19:22 +0200

    many changes in java code

---
+++ TryingFog/src/java/Data/Test.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data;
+
+/**
+ *
+ * @author trez__000
+ */
+public class Test {
+
+    public static void main(String[] args) throws Exception {
+        UserMapper mapper = new UserMapper();
+        String data = "nothing";
+        data = mapper.getEmail("alex@alex.com");
+        System.out.println(data);
+    }
+}
--- TryingFog/src/java/Data/UserMapper.java
+++ TryingFog/src/java/Data/UserMapper.java
@@ -1,21 +1,38 @@
 package Data;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import Model.User;
+
 
 
 public class UserMapper {
     private Connection con;
     
     
-    public UserMapper() throws Exception {
+    public UserMapper()  {
         con = new DB().getConnection();
     }
+    DB db = new DB();
+    public String getEmail (String email) throws SQLException {
+        String userEmail = null;
+        try {
+            Connection conn = db.getConnection();
+            PreparedStatement statement = conn.prepareStatement("select email from users where email = "+"'"+email+"'");
+            ResultSet result = statement.executeQuery();
+            if (result.next()) {
+                userEmail = result.getString("email");
+            }
+        } catch (SQLException x) {
+            System.out.println("Couldn't get email "+ x);
+        }
+        return userEmail;
+    }
     
-    public String getEmail (String email){
+    
+    public String getEmaill (String email){
         String userEmail = null;
         ResultSet rs = null;
         Statement stmt = null;        
@@ -28,7 +45,7 @@ public class UserMapper {
                 userEmail = rs.getString("email");
             }
         } catch(SQLException x) {
-            System.out.println("Email not found");
+            System.out.println("Email not found " + x);
         }
         return userEmail;
     }
@@ -54,31 +71,5 @@ public class UserMapper {
     
     
     
-    public User getUser(int id) {
-        ResultSet rs = null;
-        Statement stmt = null;
-        User user = null;
-
-        String SQLString
-                = "select * from members where id = " + id;
-        try {
-            stmt = con.createStatement();
-            rs = stmt.executeQuery(SQLString);
-            if (rs.next()) {
-                user = new User(id,
-                        rs.getString(2),
-                        rs.getString(3),
-                        rs.getString(4),
-                        rs.getString(5),
-                        rs.getString(6),
-                        rs.getString(7),
-                        rs.getString(8),
-                        Integer.parseInt(rs.getString(9)));
-            }
-        } catch (SQLException e) {
-            System.out.println("Fail in UserMapper - getUser");
-            System.out.println(e.getMessage());
-        }
-        return user;
-    }
+    
 }
